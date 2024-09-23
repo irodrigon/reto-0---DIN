a3a916aa43e0249b5d60a20381447af421a93ac5
irodrigon <irodrigon2313@gmail.com>
2024-09-23 19:42:03 +0200

    fixed database not closing connections + exceptions

--- HelloTiersWorldAitorInigo/src/DataAccessTier/DBUserDataAccessor.java
+++ HelloTiersWorldAitorInigo/src/DataAccessTier/DBUserDataAccessor.java
@@ -9,49 +9,79 @@ import Model.User;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-
-
+import java.util.logging.Logger;
 
 /**
  *
  * @author Iñi
  */
-public class DBUserDataAccessor implements DataAccessible{
-    
+public class DBUserDataAccessor implements DataAccessible {
+
     private Connection con = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
+
     private User userData;
     private final String USER_DATA = "SELECT * FROM USER";
-    
+
     @Override
     public User lookUserData() {
-       
-       ResourceBundle bundle = ResourceBundle.getBundle("resources/selection");
-       String driver = bundle.getString("driver");
-       String url = bundle.getString("url");
-       String user = bundle.getString("user");
-       String pass = bundle.getString("pass");
-       try{
+
+        ResourceBundle bundle = ResourceBundle.getBundle("resources/selection");
+        String driver = bundle.getString("driver");
+        String url = bundle.getString("url");
+        String user = bundle.getString("user");
+        String pass = bundle.getString("pass");
+        try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url,user,pass);
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(USER_DATA);
-            
-            if(rs.next()){
+            con = DriverManager.getConnection(url, user, pass);
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(USER_DATA);
+
+            if (rs.next()) {
                 userData = new User();
                 userData.setId(rs.getString("id"));
                 userData.setName(rs.getString("name"));
                 userData.setLastName(rs.getString("lastname"));
                 userData.setTelephone(rs.getString("telephone"));
                 userData.setEmail(rs.getString("email"));
-                
+
             }
-            
-       }catch(Exception e){
-           e.printStackTrace();
-       }
-        
-       return userData;
+
+        } catch (SQLException e) {
+            Logger.getLogger("DataAccessTier").severe(e.getLocalizedMessage());
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger("DataAccessTier").severe(e.getLocalizedMessage());
+        } finally {
+
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                Logger.getLogger("DataAccessTier").severe(e.getLocalizedMessage());
+            }
+
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                Logger.getLogger("DataAccessTier").severe(e.getLocalizedMessage());
+            }
+
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                Logger.getLogger("DataAccessTier").severe(e.getLocalizedMessage());
+            }
+        }
+
+        return userData;
     }
 }
